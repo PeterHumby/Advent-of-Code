11bf019c3dc66f2243d3adf15e04c71797dc13ea
PeterHumby <humbypeter@gmail.com>
2025-08-23 19:42:21 +0100

    Added day 1.

--- 2022/Day 01/Day 01.py
+++ 2022/Day 01/Day 01.py
@@ -1,2 +1,8 @@
-path = __file__[:__file__.rfind('\\')] + '\\'
+path = __file__[:__file__.rfind(__file__[0])] + __file__[0]
 file = open(path + "Input.txt", "r")
+
+
+elves = sorted([sum([int(n) for n in line.split('\n')]) for line in file.read().split('\n\n')])
+
+print("Part 1: ", elves[-1])
+print("Part 2: ", elves[-1] + elves[-2] + elves[-3])
