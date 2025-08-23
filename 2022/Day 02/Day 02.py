218fa40086c8baca8e6d76b804869c5c8b6bf12d
PeterHumby <humbypeter@gmail.com>
2025-08-23 20:40:04 +0100

    Added day 2.

--- 2022/Day 02/Day 02.py
+++ 2022/Day 02/Day 02.py
@@ -1,2 +1,30 @@
-path = __file__[:__file__.rfind('\\')] + '\\'
+path = __file__[:__file__.rfind(__file__[0])] + __file__[0]
 file = open(path + "Input.txt", "r")
+
+data = file.read().split('\n')
+
+p1_tot, p2_tot = 0, 0
+play_map = {'A': 'R', 'B': 'P', 'C': 'S', 'X': 'R', 'Y': 'P', 'Z': 'S'}
+score_map = {'R': 1, 'P': 2, 'S': 3}
+win_map = {'R': 'S', 'P': 'R', 'S': 'P'}
+lose_map = {'S': 'R', 'R': 'P', 'P': 'S'}
+for _, line in enumerate(data):
+    opp, play = line.split(' ')
+    opp, play = play_map[opp], play_map[play]
+    # Part 1
+    p1_tot += score_map[play]
+    if play == opp:
+        p1_tot += 3
+    elif win_map[play] == opp:
+        p1_tot += 6
+
+    # Part 2
+    if play == 'R':
+        p2_tot += score_map[win_map[opp]]
+    elif play == 'P':
+        p2_tot += score_map[opp] + 3
+    elif play == 'S':
+        p2_tot += score_map[lose_map[opp]] + 6
+
+print("Part 1: ", p1_tot)
+print("Part 2: ", p2_tot)
