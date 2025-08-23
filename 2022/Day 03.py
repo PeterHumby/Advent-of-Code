ee6257c46e6d539c7e8239da5f861c0c09ba5995
PeterHumby <humbypeter@gmail.com>
2025-08-23 22:11:21 +0100

    Added 2022\Day 3.

--- 2022/Day 03.py
+++ 2022/Day 03.py
@@ -2,8 +2,24 @@ file = open(r"2022\Inputs\Day 03 Input.txt", "r")
 
 data = file.read().split('\n')
 
-for _, rucksack in enumerate(data):
-    left = rucksack[:(len(rucksack) // 2)]
+priority = {c: p + 1 for p, c in enumerate(list(map(chr, range(ord('a'), ord('z')+1))) + list(map(chr, range(ord('A'), ord('Z')+1))))}
 
+p1_tot, p2_tot = 0, 0
 
-print('ABCD'[:(len('ABCD') // 2)])
+mem = []
+
+for i, rucksack in enumerate(data):
+    mem.append(set(rucksack))
+    left, right = rucksack[:(len(rucksack) // 2)], rucksack[(len(rucksack) // 2):]
+
+    overlaps = set(left).intersection(set(right))
+
+    p1_tot += sum([priority[c] for c in overlaps])
+
+    if (i + 1) % 3 == 0:
+        p2_tot += priority[mem[0].intersection(mem[1]).intersection(mem[2]).pop()]
+        mem = []
+
+print("Part 1: ", p1_tot)
+print("Part 2: ", p2_tot)
+
